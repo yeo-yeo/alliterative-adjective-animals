d79714ce6193700dce74fea4e6a58ef7bc1f16ef
Gillian <gillian@yeomans.org.uk>
2021-01-27 20:05:34 +0000

    export fn

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const animals = require('animals')
 const pluralize = require('pluralize')
 const { shuffle } = require('./utils') 
 
-const shuffledAdjectives = shuffle(adjectives)
+const generatePhrase = () => {
+    const shuffledAdjectives = shuffle(adjectives)
 
-const animal = animals();
-const plAnimal = pluralize(animal)
+    const animal = animals();
+    const plAnimal = pluralize(animal)
 
-const alliterativeAdjectives = shuffledAdjectives.filter(adj => adj[0] === plAnimal[0])
-console.log(alliterativeAdjectives[0], alliterativeAdjectives[1], plAnimal)
+    const alliterativeAdjectives = shuffledAdjectives.filter(adj => adj[0] === plAnimal[0])
+    return `${alliterativeAdjectives[0]} ${alliterativeAdjectives[1]} ${plAnimal}`
+}
+
+module.exports = generatePhrase

c811c912a3d6a391d10fa88bcdec64ba66e43280
Gillian <gillian@yeomans.org.uk>
2021-01-29 09:47:15 +0000

    capitalise

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const generatePhrase = () => {
     const plAnimal = pluralize(animal)
 
     const alliterativeAdjectives = shuffledAdjectives.filter(adj => adj[0] === plAnimal[0])
-    return `${alliterativeAdjectives[0]} ${alliterativeAdjectives[1]} ${plAnimal}`
+    const phrase = [alliterativeAdjectives[0], alliterativeAdjectives[1], plAnimal]
+    const capitalisedPhrase = phrase.map((word) => word[0].toUpperCase() + word.substring(1)).join(" ");
+
+    return capitalisedPhrase
 }
 
 module.exports = generatePhrase

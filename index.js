3df98c3281553f3994f627b2090714682ad2288c
Gillian <gillian@yeomans.org.uk>
2024-08-22 21:03:21 +0100

    be less dumb

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 import adjectives from "adjectives";
-import animals from "animals";
+import getRandomAnimal from "animals";
 import pluralize from "pluralize";
-import { shuffle } from "./utils.js";
+
+const getTwoRandomAdjs = (array) => {
+  if (array.length < 2) {
+    throw new Error("Oh no no");
+  }
+
+  const first = array[Math.floor(Math.random() * array.length)];
+  let second = first;
+
+  while (first === second) {
+    second = array[Math.floor(Math.random() * array.length)];
+  }
+  return [first, second];
+};
 
 export const generatePhrase = () => {
-  const shuffledAdjectives = shuffle(adjectives);
+  const plAnimal = pluralize(getRandomAnimal());
 
-  const animal = animals();
-  const plAnimal = pluralize(animal);
-
-  const alliterativeAdjectives = shuffledAdjectives.filter(
+  const alliterativeAdjectives = adjectives.filter(
     (adj) => adj[0] === plAnimal[0]
   );
-  const phrase = [
-    alliterativeAdjectives[0],
-    alliterativeAdjectives[1],
-    plAnimal,
-  ];
-  const capitalisedPhrase = phrase
+
+  const [adj1, adj2] = getTwoRandomAdjs(alliterativeAdjectives);
+
+  const capitalisedPhrase = [adj1, adj2, plAnimal]
     .map((word) => word[0].toUpperCase() + word.substring(1))
     .join(" ");
 
